3b1053f512d6fa9d09db0ae516e8617a17d32a0d
jazzsir <jazzsir@gmail.com>
2022-09-02 21:24:14 +0900

    managing data but display-the-cart-items failed.

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,7 +3,7 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
